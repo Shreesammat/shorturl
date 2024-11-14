ef9f0a8238c0dfcf68c8b06ce4f8bfaa8d6d0e2d
Shreesammat <shreesammat@gmail.com>
2024-11-14 13:34:39 +0530

    url validation + hash 2.0

--- backend/backend.js
+++ backend/backend.js
@@ -1,89 +1,81 @@
 const express = require('express');
 const { Url } = require('./db/connectdb');
 const crypto = require('crypto');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static('../frontend/dist'));
+app.use(express.static(path.join(__dirname, '../frontend/dist'))); // Serve React build files
 
-
-function isValidUrl(req,res,next) {
+// URL validation middleware
+function isValidUrl(req, res, next) {
   let inputUrl = req.params.url;
 
   try {
     const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[^\s]*)?$/;
-  
+
     // Prepend 'http://' if the URL lacks a protocol
     if (!/^https?:\/\//i.test(inputUrl)) {
-      url = 'http://' + inputUrl;
+      inputUrl = 'http://' + inputUrl;
     }
 
-    if(!urlPattern.test(inputUrl)) {
-      throw new Error("Invalid URL format")
+    if (!urlPattern.test(inputUrl)) {
+      throw new Error("Invalid URL format");
     }
 
-    console.log(inputUrl, ' isvalidurl try block');
+    console.log(inputUrl, 'is valid in try block');
     req.validateUrl = inputUrl;
     next();
-  } catch {
-    console.log(inputUrl,'isValidUrl catch block');
+  } catch (error) {
+    console.log(inputUrl, 'is invalid in catch block');
     res.status(400).send("Invalid URL format");
   }
-};
+}
 
+// Serve React app for root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
-app.get("/",(req,res) => {
-  res.render('index.html')
-})
-
-
-app.get("/:hash",async (req,res) => {
-  console.log('randi')
-  const {hash} = req.params;
-  console.log(hash);
-
-  const original = await Url.findOne({shortUrl: hash});
-  console.log(original);
-  const originalUrl = original.originalUrl;
-
-  if(originalUrl) {
-    res.redirect(originalUrl);
+// Redirect endpoint
+app.get("/:hash", async (req, res) => {
+  const { hash } = req.params;
+  const original = await Url.findOne({ shortUrl: hash });
+  
+  if (original && original.originalUrl) {
+    res.redirect(original.originalUrl);
   } else {
-    res.status(500).send("Invalid URL");
+    res.status(404).send("URL not found");
   }
-})
+});
 
+// URL shortening endpoint
+app.post("/shorten/:url", isValidUrl, async (req, res) => {
+  const url = req.validateUrl;
+  const existingEntry = await Url.findOne({ originalUrl: url });
 
-app.post("/shorten/:url",isValidUrl ,async (req,res) => {
-  console.log('enetered shorten')
-  const url= req.validateUrl;
-  if(!url || typeof url !== 'string') {
-    return res.status(400).send("Invalid URL");
-  }
-  const result = hasher(url);
-
-  const isExist = await Url.findOne({originalUrl:url})
-  console.log(isExist);
-
-  if(isExist) {
-    res.send({shortUrl: `https://shorturl-7tor.onrender.com/${isExist.shortUrl}`})
+  if (existingEntry) {
+    res.send({ shortUrl: `https://shorturl-7tor.onrender.com/${existingEntry.shortUrl}` });
     return;
   }
+
+  const shortUrl = hasher(url);
+  const newEntry = new Url({ originalUrl: url, shortUrl });
   
-  const urltemp = new Url({originalUrl: url, shortUrl: result})
-  await urltemp.save()
-    .then(() => console.log('updated db'))
-    .catch(() => console.log('db not updated'))
-  const shortUrl = `https://shorturl-7tor.onrender.com/${result}`;
-  res.send({shortUrl});
-})
+  try {
+    await newEntry.save();
+    console.log('DB updated with new entry');
+    res.send({ shortUrl: `https://shorturl-7tor.onrender.com/${shortUrl}` });
+  } catch (error) {
+    console.error('DB not updated', error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-//hashing
+// Hashing function for URL shortening
 const base62chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-// Function to convert a number to Base62
 function toBase62(num) {
   let base62 = '';
   while (num > 0) {
@@ -94,17 +86,12 @@ function toBase62(num) {
 }
 
 function hasher(url) {
-  // Create an MD5 hash of the URL
   const hash = crypto.createHash('md5').update(url).digest('hex');
-
-  // Convert a portion of the hash to a number and then to Base62
-  const hashInt = parseInt(hash.slice(0, 8), 16); // Take the first 8 characters of the hash
+  const hashInt = parseInt(hash.slice(0, 8), 16);
   const base62String = toBase62(hashInt);
-
-  // Return the first 7 characters of the Base62 string
   return base62String.slice(0, 7);
 }
 
 app.listen(port, () => {
-  console.log(`server running completely fine on ${port}`)
-})
+  console.log(`Server running completely fine on port ${port}`);
+});
