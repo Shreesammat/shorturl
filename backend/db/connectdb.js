bae6581e9f503186fb284b2a5c72b7fc824e8305
Shreesammat <shreesammat@gmail.com>
2024-12-10 12:19:14 +0530

    added zod and optimized code

--- backend/db/connectdb.js
+++ backend/db/connectdb.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const env = require('dotenv').config;
 
-const mongoUrl = "mongodb+srv://streamdroid12:2qKSzrxQzFPFI5Y5@cluster0.qizs1.mongodb.net/mydb"
+const mongoUrl = 'mongodb+srv://streamdroid12:2qKSzrxQzFPFI5Y5@cluster0.qizs1.mongodb.net/mydb'
 
 mongoose.connect(mongoUrl).then(() => console.log('connected successfully')).catch(e => console.log('error',e) );
 
